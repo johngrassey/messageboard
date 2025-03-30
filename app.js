a146be8c81d251cdb83ac86dc786092458f2c0ca
John Grassey <johngrassey@gmail.com>
2025-03-30 12:06:40 -0400

    Add Index page that lists messages

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+const path = require("node:path");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 const indexRouter = require("./routes/indexRouter");
 const newRouter = require("./routes/newRouter");
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,7 +1,27 @@
 const { Router } = require("express");
 
+const messages = [
+  {
+    text: "I'm looking forward to making something today!",
+    user: "John",
+    added: new Date(),
+  },
+  {
+    text: "Can't wait to see how it comes out!",
+    user: "Dave",
+    added: new Date(),
+  },
+  {
+    text: "YESSS! Finally!",
+    user: "Brielle",
+    added: new Date(),
+  },
+];
+
 const indexRouter = Router();
 
-indexRouter.get("/", (req, res) => res.send("Hello, world!"));
+indexRouter.get("/", (req, res) =>
+  res.render("index", { title: "Mini Messageboard", messages: messages })
+);
 
 module.exports = indexRouter;
